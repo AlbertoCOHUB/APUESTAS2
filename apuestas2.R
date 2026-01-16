fb867743b0d881607ba56df0a3d1c51dce03ea5a
cmadrid <cmadrid@PC785551.salud.madrid.org>
2026-01-16 11:50:33 +0100

    casas por habitante

--- apuestas2.R
+++ apuestas2.R
@@ -2,4 +2,28 @@ library(tidyverse)
 
 summary(betting)
 str(betting)
-ciudad <- 
+ciudad <- as.factor(betting$NAME)
+totalcas <- table(betting$SALONES)
+glimpse(betting)
+View(totalcas)
+View(ciudad)
+
+madrid <- read_csv("betting2_Madrid.csv")
+totalcas <- table(madrid$SALONES)
+View(totalcas)
+totalcasas <- sum(betting$SALONES, na.rm = TRUE)
+solocasa <- madrid[madrid$SALONES != 0, ]
+
+solocasa |> mutate(
+  SALONES = as.factor(SALONES))
+ 
+pobla <- solocasa |>
+  group_by(SALONES) |>
+  summarise(POPULATION = sum(POPULATION), .groups = "drop")
+
+
+
+
+                        
+                        
+                        
